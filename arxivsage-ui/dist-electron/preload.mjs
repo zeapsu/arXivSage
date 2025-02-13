6b7d3af8bf077e070ccd0ea7b92364d07d08f4c4
zeapsu <andryypaez@gmail.com>
2025-02-12 22:21:15 -0800

    inital commit

---
+++ arxivsage-ui/dist-electron/preload.mjs
@@ -0,0 +1,26 @@
+"use strict";
+const electron = require("electron");
+electron.contextBridge.exposeInMainWorld("ipcRenderer", {
+  on(...args) {
+    const [channel, listener] = args;
+    return electron.ipcRenderer.on(channel, (event, ...args2) => listener(event, ...args2));
+  },
+  off(...args) {
+    const [channel, ...omit] = args;
+    return electron.ipcRenderer.off(channel, ...omit);
+  },
+  send(...args) {
+    const [channel, ...omit] = args;
+    return electron.ipcRenderer.send(channel, ...omit);
+  },
+  invoke(...args) {
+    const [channel, ...omit] = args;
+    return electron.ipcRenderer.invoke(channel, ...omit);
+  }
+  // You can expose other APTs you need here.
+  // ...
+});
+electron.contextBridge.exposeInMainWorld("electron", {
+  sendSearchQuery: (query) => electron.ipcRenderer.send("search-articles", query),
+  onResults: (callback) => electron.ipcRenderer.on("search-results", (_, data) => callback(data))
+});
---
+++ arxivsage-ui/electron/preload.ts
@@ -0,0 +1,29 @@
+import { ipcRenderer, contextBridge } from 'electron'
+
+// --------- Expose some API to the Renderer process ---------
+contextBridge.exposeInMainWorld('ipcRenderer', {
+  on(...args: Parameters<typeof ipcRenderer.on>) {
+    const [channel, listener] = args
+    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
+  },
+  off(...args: Parameters<typeof ipcRenderer.off>) {
+    const [channel, ...omit] = args
+    return ipcRenderer.off(channel, ...omit)
+  },
+  send(...args: Parameters<typeof ipcRenderer.send>) {
+    const [channel, ...omit] = args
+    return ipcRenderer.send(channel, ...omit)
+  },
+  invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
+    const [channel, ...omit] = args
+    return ipcRenderer.invoke(channel, ...omit)
+  },
+
+  // You can expose other APTs you need here.
+  // ...
+})
+
+contextBridge.exposeInMainWorld("electron", {
+  sendSearchQuery: (query: string) => ipcRenderer.send("search-articles", query),
+  onResults: (callback: (data: any) => void) => ipcRenderer.on("search-results", (_, data) => callback(data)),
+});
