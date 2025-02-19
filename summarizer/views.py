605f383617494ac158da104645dac78d34b51360
zeapsu <andryypaez@gmail.com>
2025-02-18 21:43:10 -0800

    add deps, prototype fn's

--- summarizer/views.py
+++ summarizer/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import arxiv, json
+from openai import OpenAI
+
+
+# arxiv api to get data
+def search() -> str:
+    pass
+    
+# deepseek api to summarize 
+def summarize() -> str:
+    pass
 
 # Create your views here.
