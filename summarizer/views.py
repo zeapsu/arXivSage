f861da5f4e77345b71ed982292b9855937c94dc6
zeapsu <andryypaez@gmail.com>
2025-03-10 16:45:35 -0700

    add env vars

--- summarizer/views.py
+++ summarizer/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 import arxiv, json
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+DEEPSEEK_API_KEY = os.environ("DEEPSEEK_API_KEY")
 
 # arxiv api to get data
 def search() -> str:
@@ -9,6 +14,7 @@ def search() -> str:
     
 # deepseek api to summarize 
 def summarize() -> str:
+    client = OpenAI(api_key=, base_url="https://api.deepseek.com")
     pass
 
 # Create your views here.
